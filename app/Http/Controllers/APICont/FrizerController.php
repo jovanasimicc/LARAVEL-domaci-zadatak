509137810fb1e721105151c0daead364f398acbb
jovanasimicc <simicjovana015@gmail.com>
2023-01-13 00:31:13 +0100

    API rute i API kontroleri

---
+++ app/Http/Controllers/APICont/FrizerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\APICont;
+
+use App\Http\Controllers\Controller;
+use App\Models\Frizer;
+use Illuminate\Http\Request;
+
+class FrizerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Frizer  $frizer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Frizer $frizer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Frizer  $frizer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Frizer $frizer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Frizer  $frizer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Frizer $frizer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Frizer  $frizer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Frizer $frizer)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APICont\FrizerController;
+use App\Http\Controllers\APICont\MusterijaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('frizer', [FrizerController::class, 'index']);
+Route::get('musterija', [MusterijaController::class, 'index']);
+Route::get('frizer/{frizer}', [FrizerController::class, 'show']);
+Route::get('musterija/{musterija}', [MusterijaController::class, 'show']);
+Route::post('musterija', [MusterijaController::class, 'store']);
+Route::delete('musterija/{musterija}', [MusterijaController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
