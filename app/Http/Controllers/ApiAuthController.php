444084cd5def3ad86f6bd7fe176a4ac059232674
jovanasimicc <simicjovana015@gmail.com>
2023-01-13 22:28:10 +0100

    Autentifikacija API ruta

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+
+class ApiAuthController extends Controller
+{
+
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string',
+        ]);
+
+
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+
+        $user_id = DB::table('users')->insertGetId([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $new_user = DB::table('users')->where('id', $user_id)->first();
+
+
+        return response()->json($new_user);
+    }
+
+
+
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json('Login neuspesan');
+        }
+
+
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+
+        $login_token = $user->createToken('login_token')->plainTextToken;
+
+
+
+        return response()->json([
+            'User' => $user,
+            'Token' => $login_token,
+        ]);
+    }
+
+
+
+
+    public function logout()
+    {
+
+        auth()->user()->tokens()->delete();
+
+        return response()->json("User logout");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APICont\FrizerController;
 use App\Http\Controllers\APICont\MusterijaController;
+use App\Http\Controllers\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,19 @@ Route::get('frizer', [FrizerController::class, 'index']);
 Route::get('musterija', [MusterijaController::class, 'index']);
 Route::get('frizer/{frizer}', [FrizerController::class, 'show']);
 Route::get('musterija/{musterija}', [MusterijaController::class, 'show']);
-Route::post('musterija', [MusterijaController::class, 'store']);
-Route::delete('musterija/{musterija}', [MusterijaController::class, 'destroy']);
+
+
+Route::post('register', [ApiAuthController::class, 'register']);
+Route::post('login', [ApiAuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('musterija', [MusterijaController::class, 'store']);
+    Route::delete('musterija/{musterija}', [MusterijaController::class, 'destroy']);
+    Route::post('logout', [ApiAuthController::class, 'logout']);
+});
+
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
